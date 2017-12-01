19c0f226c2d1c8955604a4f3d24a49e3f5cc127d
Bryce Heltzel <bheltzel@gatech.edu>
2017-12-01 13:52:08 -0800

    Added home page, feedback bot, change log

---
+++ public/feedback.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bryce
+ * Date: 12/1/2017
+ * Time: 1:35 PM
+ */
--- public/index.php
+++ public/index.php
@@ -3,79 +3,30 @@
 require_once("header.php");
 ?>
 <body>
-
 <div class="container-fluid" style="margin-top: 60px;">
-    <div class="row" style="padding: 0 0 5px 0;">
-        <div class="col-xs-12 col-lg-7 col-md-7">
-            <input id="search" class="search form-control" placeholder="Search..." style="">
-        </div>
-        <div class="col-xs-12 col-lg-4 col-md-4" style="margin-left: 10px; display: inline-block;">
-            <input id="1" type="checkbox" class="position-checkbox" name="position-checkbox" checked/>
-            <label for="1">GK</label>
-            <input id="2" type="checkbox" class="position-checkbox" name="position-checkbox" checked/>
-            <label for="2">DEF</label>
-            <input id="3" type="checkbox" class="position-checkbox" name="position-checkbox" checked/>
-            <label for="3">MID</label>
-            <input id="4" type="checkbox" class="position-checkbox" name="position-checkbox" checked/>
-            <label for="4">FWD</label>
+    <div class="row">
+        <div class="col-xs-12">
+        <h2>Using FNTSY PL</h2>
+        <p>FNTSY PL currently contains six different tools.</p>
+        <ul>
+            <li><b>Live Table:</b> The only live table tool currently available that updates within minutes! Input your Mini-League ID to get live updates of who is playing and their point totals during a gameweek. League ID can be found in the URL of the League page on FPL. Only the top 50 teams in the League are considered.</li>
+            <ul>
+                <li>Bookmark this page with your League ID!<br><a href="http://fntsypl.com/live?league=00000" target="_blank">http://fntsypl.com/live?league=00000</a></li>
+                <li>Search for Player Names in order to filter on teams with a player!</li>
+            </ul>
+            <li><b>Selections:</b> Input your Mini-League ID to get a list of selected players for your league. League ID can be found in the URL of the League page on FPL. Compare your league ownership to overall ownership. Only the top 50 teams in the League are considered.</li>
+            <ul><li>Bookmark this page with your League ID<br><a href="http://fntsypl.com/selections?league=00000" target="_blank">http://fntsypl.com/selections?league=00000</a></li></ul>
+            <li><b>Players:</b> A comprehensive list of statistics at the individual player level. These statistics come from the FPL API except for VAPM which is calculated here. The next two fixtures for each player are also displayed and formatted based on difficulty (according to FPL).</li>
+            <li><b>Fixtures:</b> An outline of upcoming fixtures based on difficulty. The formatting is sourced by the underlying numbers used by FPL. Home and Away form is taken into consideration. The Overall number is an equally weighted average among the number of fixtures selected in the Gameweeks dropdown.</li>
+            <li><b>My Team (BETA):</b> A team planner for the next four gameweeks. Click players to turn their background green in order to plan a starting lineup. Click again to turn red to plan a transfer. Click a third time to reset. Future plans included incorporating budget and transfers.</li>
+            <ul><li>Bookmark this page with your Team ID!<br><a href="http://fntsypl.com/my?team=00000" target="_blank">http://fntsypl.com/my?team=00000</a></li></ul>
+            <li><b>Teamsheets (BETA):</b> Projected teamsheets for each team, along with PPG. Disclaimer: this is not always completely up to date and is a work in progress for formatting.</li>
+        </ul>
         </div>
     </div>
     <div class="row">
-        <div class="col-xs-12 table-responsive" id="player">
-            <table id="players" class="table table-striped table-sm"></table>
-        </div>
+
     </div>
 </div>
-<script>
-
-    $(document).ready(function() {
-        get_players();
-
-        $('.position-checkbox').change(function() {
-            var position = $(this).attr('id');
-            if (this.checked) {
-                $('tr[position=' + position + ']').show();
-            } else {
-                $('tr[position=' + position + ']').hide();
-            }
-        });
-    });
-
-    function get_players() {
-        var positions = new Array();
-        $("input:checkbox[name=position-checkbox]:checked").each(function(){
-            positions.push($(this).attr('id'));
-        });
-
-        var data = {mode: 'players', positions: positions};
-
-        $.post('data/from_db.php', data, function(resp) {
-            var table = $('#players');
-            var j = JSON.parse(resp);
-            table.html(j.BODY);
-
-            var dataTable = table.DataTable({
-                "order": [3, "desc"]
-                , "paging": false
-                //,"scrollY": "95%"
-                //,"scrollX": "auto"
-                , "info": false
-                , "stateSave": true
-                , "compact": false
-                , "dom": 'ltipr'
-                , "searching": true
-                , "destroy": true
-            });
-
-            $('#search').on( 'keyup', function () {
-                dataTable.search( this.value ).draw();
-            });
-        });
-    }
-
-
-</script>
 </body>
-
-
 </html>
